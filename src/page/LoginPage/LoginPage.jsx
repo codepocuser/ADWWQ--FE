5d6597612f89a805409992f7cbceba3f76553c14
Billy Sin <>
2023-07-19 15:46:11 +0800

    remove logo

--- src/page/LoginPage/LoginPage.jsx
+++ src/page/LoginPage/LoginPage.jsx
@@ -42,9 +42,9 @@ const LoginPage = () => {
   return (
 
       <div className="login-page">
-        <div className="login-header">
-          <img src="https://www.cncbinternational.com/_images/logo-new.gif" className="bank-logo" alt="logo" />
-        </div>
+        {/*<div className="login-header">*/}
+        {/*  <img src="https://www.cncbinternational.com/_images/logo-new.gif" className="bank-logo" alt="logo" />*/}
+        {/*</div>*/}
         {counter > 0 ?
             <>
               <QRCodeComponent codes={qrcode} />
